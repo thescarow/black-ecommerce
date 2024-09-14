0abc4ddabbc05ea8f5be8c2795738e84af882add
Rithik Pathak <rithikpathak123@gmail.com>
2024-09-15 00:36:00 +0530

    changed code for deploy v01

---
+++ api/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+
+const chalk = require('chalk')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const keys = require('./config/keys')
+const routes = require('./routes')
+const socket = require('./socket')
+const setupDB = require('./utils/db')
+
+const { port } = keys
+const app = express()
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')))
+////////////////////////////////////////////////////////////
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    frameguard: true
+  })
+)
+app.use(cors())
+
+setupDB()
+require('./config/passport')(app)
+app.use(routes)
+// All other GET requests should return the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'))
+})
+
+const server = app.listen(port, () => {
+  console.log(
+    `${chalk.green('✓')} ${chalk.blue(
+      `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+    )}`
+  )
+})
+
+socket(server)
